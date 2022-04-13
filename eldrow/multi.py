157e446b35c7654360af9c67025534270abdc124
Peter Gaultney <petergaultney@gmail.com>
2022-04-13 10:27:25 -0500

    parallelize cross-elim

--- eldrow/multi.py
+++ eldrow/multi.py
@@ -1,5 +1,7 @@
 import typing as ty
 from collections import defaultdict
+from functools import reduce, partial
+from multiprocessing import Pool
 
 from .elimination import elimination_scorer, make_options
 from .game import Game, get_options, novel_or_option, novelty
@@ -10,6 +12,47 @@ class CrossElim(ty.NamedTuple):
     elim_ratio: float
     solved: set
     option: set
+
+
+def _merge_cross_elims(
+    a: ty.Dict[str, CrossElim], b: ty.Dict[str, CrossElim]
+) -> ty.Dict[str, CrossElim]:
+    assert set(a) == set(b)
+
+    def merge(ac: CrossElim, bc: CrossElim) -> CrossElim:
+        return CrossElim(ac.elim_ratio * bc.elim_ratio, ac.solved | bc.solved, ac.option | bc.option)
+
+    return {word: merge(a[word], b[word]) for word in a}
+
+
+def _hash_game(game: Game) -> str:
+    return f"{game.n}-{game.alpha}-{len(game.wl)}-{game.guesses}"
+
+
+def caching_cross_elim_for_guesses_and_word(game: Game) -> CrossElim:
+    pass
+
+
+def elim_game(candidates: ty.Collection[str], key: str, game: Game) -> ty.Dict[str, CrossElim]:
+    opts = set(get_options(game))
+    print(key, " ".join(colorize(*game.guesses)))
+    elim_scorer = elimination_scorer(opts, make_options(game.alpha, opts, game.guesses))
+    cross_game_elimination_multipliers = dict()
+    for word in candidates:
+        elim_count = elim_scorer(word)
+        game_elim_ratio = elim_count / len(opts)
+        cross_game_elimination_multipliers[word] = CrossElim(
+            (game_elim_ratio if not len(opts) == 1 else 1.0),
+            {key} if elim_count == len(opts) - 1 else set(),
+            {key} if word in opts else set(),
+        )
+    return cross_game_elimination_multipliers
+
+
+def _p_elim_game(
+    candidates: ty.Collection[str], key_game: ty.Tuple[str, Game]
+) -> ty.Dict[str, CrossElim]:
+    return elim_game(candidates, *key_game)
 
 
 def elim_across_games(
@@ -31,25 +74,10 @@ def elim_across_games(
         w for w, s in sorted(cross_game_novelty_scores.items(), key=lambda t: t[1], reverse=True)
     ][:limit]
 
-    cross_game_elimination_multipliers: ty.Dict[str, CrossElim] = defaultdict(
-        lambda: CrossElim(1.0, set(), set())
-    )
-
-    for key, game in games.items():
-        print(key, " ".join(colorize(*game.guesses)))
-        opts = game_options[key]
-        elim_scorer = elimination_scorer(opts, make_options(game.alpha, opts, game.guesses))
-        for word in best_novelty_words:
-            opt_inc = {key} if word in opts else set()
-            elim_count = elim_scorer(word)
-            game_elim_ratio = elim_count / len(opts)
-            solved = {key} if elim_count == len(opts) - 1 else set()
-            cross_elim = cross_game_elimination_multipliers[word]
-            cross_game_elimination_multipliers[word] = CrossElim(
-                cross_elim.elim_ratio * (game_elim_ratio if not len(opts) == 1 else 1.0),
-                cross_elim.solved | solved,
-                cross_elim.option | opt_inc,
-            )
+    with Pool(len(games)) as pool:
+        cross_game_elimination_multipliers = reduce(
+            _merge_cross_elims, pool.map(partial(_p_elim_game, best_novelty_words), games.items())
+        )
 
     sorted_by_options = sorted(
         cross_game_elimination_multipliers.items(), key=lambda kv: len(kv[1].option)
